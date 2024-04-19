50e7cec4aea2fb323f33285e421956ec9bb69994
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-20 00:19:17 +0530

    fix issues with the admin sidebar

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import AdminNavbar from '@/components/AdminNavbar'; // Ensure correct casing and path
+
 
 const Page = () => {
   return (
     <div>
-      <AdminNavbar />
+    <h1>hello</h1>
     </div>
   );
 }
