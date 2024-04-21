832574d383804a54c5a4e35602c2cd6a10a5e667
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-21 21:33:24 +0530

    Fixed bugs in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com', 'images.wiloke.com', 'plugins.wiloke.com' , 'plus.unsplash.com' , "via.placeholder.com" ]
-      },
-      reactStrictMode: true,
-      env: {
-        domain: 'https://theroyal-niks-nikhil-anands-projects.vercel.app/',
-      },
+  images: {
+    domains: ['images.unsplash.com', 'images.wiloke.com', 'plugins.wiloke.com', 'plus.unsplash.com', 'via.placeholder.com'],
+  },
+  reactStrictMode: true,
+  env: {
+    domain: 'https://theroyal-niks-nikhil-anands-projects.vercel.app/',
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
