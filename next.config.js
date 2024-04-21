5f5e1dff8928eb15611994418cd00fac8251d816
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-21 21:37:47 +0530

    fix typo

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   env: {
     domain: 'https://theroyal-niks-nikhil-anands-projects.vercel.app/',
   },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, './src');
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
