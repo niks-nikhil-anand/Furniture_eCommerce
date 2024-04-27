6d362613d63916fd1e0ec0ed331bd6f6a3922e9c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-27 11:12:45 +0530

    Fix some padding

--- src/app/(Categories)/categories/page.jsx
+++ src/app/(Categories)/categories/page.jsx
@@ -5,17 +5,20 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className='p-[3rem]'>
+    <>
+     <div className='p-[3rem]'>
       
       <div className='p-5 flex flex-wrap gap-5 '>
         {categories2.map((category2) => (
           <CategoryCard key={category2.id} title={category2.name} image={category2.image} />
         ))}
       </div>
-      <div>
+</div>
+ <div>
       <Card/>
       </div>
-    </div>
+    </>
+   
   );
 };
 
